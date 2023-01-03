9710555254df10cab09cbdf0246d6bd13adbea7d
munoz213 <118865232+munoz213@users.noreply.github.com>
2023-01-02 14:27:05 -1100

    bdd

--- quiz/routes/student.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router()
-const studentController  = require('../controller/studentController');
-
-
-router.get('/getallquiz',studentController.getallquiz),
-router.get('/getallquestion/:id',studentController.getAllQuestion);
-
-module.exports = router
