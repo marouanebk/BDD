c0d3736ae6ded7dc7472e27cb4e9722f7a4e6eae
borhxneddine <101331008+borhxneddine@users.noreply.github.com>
2023-01-21 13:29:37 +0100

    counter

---
+++ backend/models/model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import uniqueValidator from"mongoose-unique-validator";
+
+const UserSchema = new mongoose.Schema({
+  userid: {
+    type: String,
+    required: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }, type: {
+    type: String,
+    default: "Student",
+  },
+  biography: {
+    type: String,
+    required: false
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+/**
+ *  Here we are creating and setting an id property and 
+    removing _id, __v, and the password hash which we do not need 
+    to send back to the client.
+ */
+UserSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    //do not reveal passwordHash
+    delete returnedObject.password;
+},
+});
+
+
+/**
+ * 1. The userSchema.plugin(uniqueValidator) method won’t let duplicate email id to be stored in the database.
+ * 2. The unique: true property in email schema does the internal optimization to enhance the performance.
+*/
+
+
+
+UserSchema.plugin(uniqueValidator, { message: "Email already in use." });
+
+
+export default  mongoose.model("user", UserSchema);
+
